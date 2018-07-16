b0974baa3f2535bf277407599326f7db372266c9
Liu JiaWei <l1uj1awe1@sina.com>
2018-07-16 17:43:01 +0800

    去除多余代码

--- app/src/main/java/com/readboyi/busalarm/controller/fragment/AddListenerFragment.kt
+++ app/src/main/java/com/readboyi/busalarm/controller/fragment/AddListenerFragment.kt
@@ -2,18 +2,11 @@ package com.readboyi.busalarm.controller.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.widget.DefaultItemAnimator
-import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.orhanobut.logger.Logger
 import com.readboyi.busalarm.R
-import com.readboyi.busalarm.adapter.BusListenerAdapter
-import com.readboyi.busalarm.data.bean.BusListenerBean
 import com.readboyi.busalarm.data.database.BusDBManager
-import kotlinx.android.synthetic.main.fragment_bus_listener.*
-import java.util.ArrayList
 
 class AddListenerFragment : Fragment() {
 
