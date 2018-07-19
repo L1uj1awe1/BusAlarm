9efb8a25b49494aac7ef2336347c8a4b4e3ba645
Liu JiaWei <l1uj1awe1@sina.com>
2018-07-19 11:40:43 +0800

    主题

---
+++ app/src/main/java/com/readboyi/busalarm/data/bean/BusAppTheme.kt
@@ -0,0 +1,26 @@
+package com.readboyi.busalarm.data.bean
+
+/**
+ * Created by liujiawei on 18-7-19.
+ */
+object BusAppTheme {
+
+    object BusListener {
+
+        object Sun {
+            val actionBar = "#FFFFFF"
+            val busKey = "#2EE4A3"
+            val contentBackgroud = "#D6E9ED"
+            val contentItem = "#FFFFFF"
+        }
+
+        object Moon {
+            val actionBar = "#1F253D"
+            val busKey = "#FA4865"
+            val contentBackgroud = "#0F152F"
+            val contentItem = "#1F253D"
+        }
+
+    }
+
+}
